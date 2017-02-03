dcf012687b42597281cf20066f397aac02b32674
Jonathan Clem <jonathan@jclem.net>
2017-02-03 12:08:11 -0500

    Allow releasing of branches

--- lib/po/command/release.ex
+++ lib/po/command/release.ex
@@ -16,13 +16,21 @@ defmodule Po.Command.Release do
   Triggers a release from GitHub.
   """
   @spec run(Po.MessageHandler.tokens, Po.Slack.event, Po.Slack.slack) :: any
+  def run([app_name, branch, env], message, slack) do
+    do_run({app_name, branch, env}, message, slack)
+  end
+
   def run([app_name, env], message, slack) do
+    do_run({app_name, "master", env}, message, slack)
+  end
+
+  defp do_run({app_name, branch, env}, message, slack) do
     RegisteredApp
     |> where(alias: ^app_name)
     |> Repo.one
     |> case do
       app = %RegisteredApp{} ->
-        {:ok, build} = create_build(app, env)
+        {:ok, build} = create_build(app, branch, env)
 
         build["output_stream_url"]
         |> LogStreamer.stream(message.channel, window_size: 20)
@@ -34,9 +42,9 @@ defmodule Po.Command.Release do
     end
   end
 
-  @spec create_build(RegisteredApp.t, String.t) :: {:ok, map}
-  defp create_build(app, env) do
-    url = tarball_url(app)
+  @spec create_build(RegisteredApp.t, String.t, String.t) :: {:ok, map}
+  defp create_build(app, branch, env) do
+    url = tarball_url(app, branch)
 
     "/apps/#{app.heroku_name}-#{env}/builds"
     |> Heroku.post(%{source_blob: %{url: url}})
@@ -46,7 +54,7 @@ defmodule Po.Command.Release do
     end
   end
 
-  @spec tarball_url(RegisteredApp.t) :: String.t
-  defp tarball_url(%{github_repo: name}),
-    do: "https://#{@github_username}:#{@github_token}@github.com/usecanvas/#{name}/archive/master.tar.gz"
+  @spec tarball_url(RegisteredApp.t, String.t) :: String.t
+  defp tarball_url(%{github_repo: name}, branch),
+    do: "https://#{@github_username}:#{@github_token}@github.com/usecanvas/#{name}/archive/#{branch}.tar.gz"
 end
